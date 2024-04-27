9129fee20b11dabbfa08e2407425a2606ee26d55
Yevhen Mazur <yevhen.mazur@pm.me>
2024-04-27 20:07:14 +0200

    Add Task 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goit-pycore-hw-04
+
+Topic 6. Homework. Working with files and the modular system
---
+++ task-01/main.py
@@ -0,0 +1,28 @@
+def total_salary(path: str) -> tuple[int]:
+    '''
+    The function analyzes the file and returns the total and average salary of all developers.
+    '''
+    sum = 0
+    med = 0
+    count = 0
+    try:
+        with open(path, encoding='utf-8') as file:
+            for line in file:
+                line = line.split(",")
+                sum += int(line[1] )
+                count += 1
+            med = sum / count
+    except FileNotFoundError:
+        print("Не вдалося знайти файл.\nПеревірте його ім'я та правильність шляху")
+        return None
+    
+    except IndexError:
+        print("Не вдалося опрацювати файл!\nПеревірте що у кожній стрічці файла міститься м'я працівнка і його зарплата, розділені комою")
+        return None
+
+    return((sum, med))
+
+### Uncomment section below to check the function
+'''
+print(total_salary("task-01/text.tx"))
+'''
